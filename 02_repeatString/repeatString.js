55de64735a0fadd35d106e1773cf639f5b2a95e6
lopsang-hyolmopa <lopsang.lama.comparewheels@gmail.com>
2023-12-29 21:59:47 +0545

    Complete upto exercise 4

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  if (num >= 0) {
+    let finalString = "";
+    for (let i = 1; i <= num; i++) {
+      finalString += string;
+    }
+    return finalString;
+  } else {
+    return "ERROR";
+  }
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    // let splitedString = str.split("")
+    // let reversedArray = splitedString.reverse()
+    // let joinedArray = reversedArray.join("")
+    // let reversedString = joinedArray
+    // return reversedString
 
+    return [...str].reverse().join("")
 };
 
 // Do not edit below this line
